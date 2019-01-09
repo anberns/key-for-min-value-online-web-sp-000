a51a61f11f099d8337d7c4ea1d1ab8f9a1d3ef98
Aaron Berns <anberns@gmail.com>
2019-01-09 14:22:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,10 @@ def key_for_min_value(name_hash)
   min_value = 100000000
   min_name = nil 
   name_hash.each do |name, value|
-    min_name = name if value < min_value 
+    if value < min_value 
+      min_value = value 
+      min_name = name 
+    end
   end 
   min_name
     
